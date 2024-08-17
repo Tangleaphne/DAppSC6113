7159c10a5e2ce3c48c83855a1432abfa5e559a55
Tangleaphne <98678252+Tangleaphne@users.noreply.github.com>
2024-08-17 16:22:58 +0800

    Add files via upload

---
+++ templates/w1 storage.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8;
+
+contract store_money{
+    uint money;
+
+    function store_m(uint num) public {
+        money = num;
+    }
+
+    function view_m() public view returns (uint) {
+        return money;
+    }
+
+}
