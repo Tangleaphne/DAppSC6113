d5c1a6ee2c910e6cb01b0a1786b0e07424f8f0c1
Tangleaphne <98678252+Tangleaphne@users.noreply.github.com>
2024-09-05 14:35:37 +0000

    add admin and DB

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask,render_template,request
+import sqlite3
+import datetime
 
 app=Flask(__name__)
 
@@ -9,7 +11,41 @@ def index():
 @app.route('/main',methods=["get","post"])
 def main():
     r = request.form.get("q")
+    current_time = datetime.datetime.now()
+    conn = sqlite3.connect('dapp.db')
+    c = conn.cursor()
+    c.execute("insert into user values (?, ?)", (r, current_time))
+    conn.commit()
+    c.close()
+    conn.close()
     return render_template('main.html', r=r)
+
+@app.route('/admin',methods=["get","post"])
+def admin():
+    return render_template('admin.html')
+
+@app.route('/viewDB',methods=["get","post"])
+def viewDB():
+    conn = sqlite3.connect('dapp.db')
+    c = conn.cursor()
+    c.execute("select * from user")
+    r = ""
+    for row in c:
+        r += str(row) + "\n"
+    conn.commit()
+    c.close()
+    conn.close()
+    return render_template('viewDB.html', r=r)
+
+@app.route('/delDB',methods=["get","post"])
+def delDB():
+    conn = sqlite3.connect('dapp.db')
+    c = conn.cursor()
+    c.execute("delete from user")
+    conn.commit()
+    c.close()
+    conn.close()
+    return render_template('deleteDB.html')
 
 @app.route('/store_money',methods=["get","post"])
 def store_money():
